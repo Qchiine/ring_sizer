4500d98b4d258efd7b0291884feb2fecbcea0db0
chaimaa qchiine <162720400+Qchiine@users.noreply.github.com>
2025-11-06 23:45:51 +0100

    recrée login & register

---
+++ lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:ring_sizer/features/auth/screens/register_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Connexion'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Mot de passe',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () {
+                // TODO: Implémenter la logique de connexion
+                final email = _emailController.text;
+                final password = _passwordController.text;
+                print('Login attempt with: $email and $password');
+              },
+              child: const Text('Se connecter'),
+            ),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const RegisterScreen()),
+                );
+              },
+              child: const Text('Pas encore de compte ? S\'inscrire'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
